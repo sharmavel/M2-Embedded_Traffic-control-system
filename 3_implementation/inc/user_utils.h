5206c68c94dcd17cb551ef87223b916ae4ce6aeb
Sharma.V <94122920+sharmavel@users.noreply.github.com>
2021-12-02 13:56:15 +0530

    Update user_utils.h

--- 3_implementation/inc/user_utils.h
+++ 3_implementation/inc/user_utils.h
@@ -1,1 +1,24 @@
+#ifndef _USER_UTILS_H_
+#define _USER_UTILS_H_
+/**
+ * @file user_utils.h
+ * @author Sharmavel ()
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "traffic-signal-system.h"
+#include <util/delay.h>
 
+/**
+ * @brief Function to generate delay in micro seconds
+ * 
+ * @param[in] delay_time Delay in Micro seconds
+ * 
+ */
+void delay_ms(uint32_t delay_time);
+
+#endif /* _USER_UTILS_H_ */
